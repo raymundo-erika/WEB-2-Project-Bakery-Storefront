c2b2da11ce0b73c3d0599145910268a8e217de2f
Erika Raymundo <79664360+raymundo-erika@users.noreply.github.com>
2021-07-12 13:45:45 +0800

    2424242

--- php/displayCategories.php
+++ php/displayCategories.php
@@ -13,17 +13,11 @@ foreach($categories as $category) {
     $icon = $category->getElementsByTagName("icon")[0]->nodeValue;
     $description = $category->getElementsByTagName("description")[0]->nodeValue;
 
-    if($count==1) {
-        echo "<div id='".$id."' class='category category-clicked' onclick='showThreeProducts(this, \"".$id."\")'>
-        <div class='image' style='background-image: url(\"".$icon."\")'></div>
-        <p>".$title."</p>
-        </div>";
-    } else {
         echo "<div id='".$id."' class='category' onclick='showThreeProducts(this, \"".$id."\")'>
         <div class='image' style='background-image: url(\"".$icon."\")'></div>
         <p>".$title."</p>
         </div>";
-    }
+    
 }
 
 
--- php/navCategories.php
+++ php/navCategories.php
@@ -7,23 +7,10 @@ $categories = $xml->getElementsByTagName("category");
 $count = 0;
 
 foreach($categories as $category) {
-    $count++;
     $id = $category->getAttribute("id");
     $title = $category->getElementsByTagName("title")[0]->nodeValue;
-    $icon = $category->getElementsByTagName("icon")[0]->nodeValue;
-    $description = $category->getElementsByTagName("description")[0]->nodeValue;
 
-    if($count==1) {
-        echo "<div id='".$id."' class='category category-clicked' onclick='showThreeProducts(this, \"".$id."\")'>
-        <div class='image' style='background-image: url(\"".$icon."\")'></div>
-        <p>".$title."</p>
-        </div>";
-    } else {
-        echo "<div id='".$id."' class='category' onclick='showThreeProducts(this, \"".$id."\")'>
-        <div class='image' style='background-image: url(\"".$icon."\")'></div>
-        <p>".$title."</p>
-        </div>";
-    }
+    echo 
 }
 
 
